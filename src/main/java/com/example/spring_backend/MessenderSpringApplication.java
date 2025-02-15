880aa7430b403e27c94d84ebbffca6548909e89d
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-16 00:47:08 +0700

    Add application yaml

---
+++ src/main/java/com/example/spring_backend/MessenderSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessenderSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessenderSpringApplication.class, args);
+    }
+}
