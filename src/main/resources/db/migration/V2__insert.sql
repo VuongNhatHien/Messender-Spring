0b5621939e7f5ec7a07ff11afef4853926f2633d
Vuong Nhat Hien <hienvuongnhat@gmail.com>
2025-02-19 00:07:21 +0700

    Update seed

--- src/main/resources/db/migration/V2__insert.sql
+++ src/main/resources/db/migration/V2__insert.sql
@@ -12,4 +12,11 @@ INSERT INTO chats (user1_id, user2_id)
 VALUES
 (1, 2),
 (1, 3),
+(1, 4),
 (3, 4);
+
+-- Insert message
+INSERT INTO messages (chat_id, sender_id, message)
+VALUES
+(1, 1, 'Hello, Hien2!'),
+(2, 3, 'Hello, Hien3!');
